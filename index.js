10f6670572d95452f57749d199d1730c40cd121e
Shawshank08 <shashank08020@gmail.com>
2025-03-19 11:16:57 +0530

    .

--- index.js
+++ index.js
@@ -3,28 +3,31 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files (e.g., MP3 files) from the "songs" folder
-app.use("/songs", express.static(path.join(__dirname, "songs")));
+// Enable CORS for all routes
 const cors = require("cors");
 app.use(cors({ origin: "https://spotifycloneshashank.netlify.app" }));
+
+// Serve static files (e.g., MP3 files) from the "songs" folder
+app.use("/songs", express.static(path.join(__dirname, "songs")));
+
 // Endpoint to list songs
 app.get("/songs", (req, res) => {
-  const songsDir = path.join(__dirname, "songs");
-  const fs = require("fs");
-  fs.readdir(songsDir, (err, files) => {
-    if (err) {
-      return res.status(500).send("Unable to scan songs directory");
-    }
-    const mp3Files = files.filter(file => file.endsWith(".mp3"));
-    res.json(mp3Files);
-  });
+    const songsDir = path.join(__dirname, "songs");
+    const fs = require("fs");
+    fs.readdir(songsDir, (err, files) => {
+        if (err) {
+            return res.status(500).send("Unable to scan songs directory");
+        }
+        const mp3Files = files.filter(file => file.endsWith(".mp3"));
+        res.json(mp3Files);
+    });
 });
 
 // Route for the root URL
 app.get("/", (req, res) => {
-  res.send("Welcome to the Music Player Backend!");
+    res.send("Welcome to the Music Player Backend!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
